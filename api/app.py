40694b2014a101b992ace31543f59d91e8c68795
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-09-09 13:42:25 -0300

    :books: feat: Implementando assincronismo

--- api/app.py
+++ api/app.py
@@ -1,38 +1,33 @@
+from asyncio import to_thread
+
 from fastapi import FastAPI, status
 from fastapi.responses import RedirectResponse
-
-
 import joblib
-
+import os
 from api.schemas import PredictRequest, Message
 from data.species import species_map
 
-# load the trained model
-import os
-
+# Load the trained model
 model_path = os.path.join(os.path.dirname(__file__), "../model/model.pkl")
 model = joblib.load(model_path)
 
-
-# create instance of fastAPI
+# Create instance of FastAPI
 app = FastAPI(title="Prediction API - Iris")
 
-
 @app.get("/")
-def read_root():
+async def read_root():
     return RedirectResponse(url="/docs")
 
-
-# define the prediction endpoint
+# Define the prediction endpoint
 @app.post(
     path="/predict",
     response_model=Message,
     status_code=status.HTTP_200_OK,
     summary="Predição",
-    description="Predição do tipo de flor com base das informações de petála e sepála",
+    description="Predição do tipo de flor com base das informações de pétala e sépala",
     name="predição apí-iris",
 )
-def predict(request: PredictRequest):
+async def predict(request: PredictRequest):
     data = [
         [
             request.sepal_length,
@@ -42,6 +37,7 @@ def predict(request: PredictRequest):
         ]
     ]
 
-    prediction = model.predict(data)
+    # Execute model prediction
+    prediction = await to_thread(model.predict, data)
 
     return {"prediction": species_map[prediction[0]]}
