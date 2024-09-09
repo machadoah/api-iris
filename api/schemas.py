6e2cccf2cbc21f056ac6eb62b66b57acb190084e
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-09-09 12:38:32 -0300

    :books: feat: criando contratos

---
+++ api/schemas.py
@@ -0,0 +1,13 @@
+# define the request body for input data (features or independent variables)
+from pydantic import BaseModel
+
+
+class PredictRequest(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+class Message(BaseModel):
+    prediction: str
